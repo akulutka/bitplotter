c96d774bb07c39f00a0ec1661a9f7270c64e22e7
Ingenious <IngeniousA@yandex.ru>
2019-04-18 11:30:12 +0300

    Float plotter

--- plotter.py
+++ plotter.py
@@ -1,44 +1,81 @@
 import math
-minx = -3
-maxx = 3
+minx = -5
+maxx = 5
 
-rangex = maxx - minx + 1
+precx = 0.2
+precy = 0.01
+
+rangex = int((maxx - minx + 1) / precx)
+
+def rawf(x):
+    return math.erf(x) * math.sin(x)
 
 def plotf(x):
-    return int(math.sinh(x))
+    lminy = -9999999
+    lmaxy = -lminy
+    try:
+        f = rawf(x)
+        if f < lminy:
+            return lminy
+        elif f > lmaxy:
+            return lmaxy
+        return f
+    except Exception:
+        return None
 
 
 def minmax():
-    maxy = -9999999
-    miny = -maxy
-    for i in range(minx, maxx + 1):
+    lminy = maxy = -9999999
+    lmaxy = miny = -maxy
+    
+    i = minx
+    while i <= maxx:
         x = i
         y = plotf(x)
-        if y < miny:
-            miny = y
-        if y > maxy:
-            maxy = y
+        if y != None:
+            if lminy < y < miny:
+                miny = y
+            if lmaxy > y > maxy:
+                maxy = y
+        i += precx
     return miny, maxy
 
 
 def plot():
     miny, maxy = minmax()
-    rangey = maxy - miny + 1
+    rangey = int((maxy - miny + 1) / precy)
     array = [' '] * rangex * rangey
-    for i in range(minx, maxx + 1):
+    prev = None
+    i = minx
+    while i <= maxx:
         x = i
         y = plotf(x)
-        xind = x - minx
-        yind = maxy - y
-        index = rangex * yind + xind
-        array[index] = '#'
+        if y != None:
+            xind = int((x - minx) / precx)
+            yind = int((maxy - y) / precy)
+            index = rangex * yind + xind
+            if prev == None:
+                if plotf(x + 1) != None and y < plotf(x + 1):
+                    array[index] = '/'
+                elif plotf(x + 1) != None and y > plotf(x + 1):
+                    array[index] = '\\'
+                else:
+                    array[index] = '-'
+            elif prev < y:
+                array[index] = '/'
+            elif prev > y:
+                array[index] = '\\'
+            else:
+                array[index] = '-'
+            prev = y
+        i += precx
     return array
 
 
 def drawplot(plot):
     md = 0
     for el in plot:
-        md = (md + 1) % (maxx - minx + 1)
+        md = (md + 1) % int((maxx - minx + 1) / precx)
         print(el, end=' ')
         if md == 0:
             print()
